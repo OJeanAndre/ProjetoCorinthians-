314744a668b32419ce396f3fe8082b72fdd0026b
JE ANDRE ! <143427287+OJeanAndre@users.noreply.github.com>
2024-09-10 00:25:49 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,39 @@
+let dados = [ 
+    {
+    titulo : "Yuri Alberto",
+    descricao : " Yuri Alberto é um atacante brasileiro que tem se destacado no cenário nacional,especialmente com a camisa do Corinthians. Com uma habilidade refinada e um faro de gol apurado, ele se tornou um dos principais jogadores do time paulista. Sua velocidade, dribles desconcertantes e finalizações precisas o tornam uma ameaça constante para as defesas adversárias.A torcida corintiana tem depositado grandes expectativas em Yuri Alberto, que promete ainda mais alegrias e títulos para o clube.", 
+    link : "https://pt.wikipedia.org/wiki/Yuri_Alberto",
+    tags : "craque bom matador"
+
+    },
+    {
+    titulo : "Igor Coronado",
+    descricao : " O Maestro Corintiano Coronado é um meio-campista ítalo-brasileiro que se destaca pela sua habilidade com a bola e visão de jogo. Atualmente defendendo as cores do Corinthians, o jogador tem se mostrado fundamental para o time, com passes precisos e capacidade de criar jogadas. Sua elegância e técnica refinada o tornam um dos destaques do time paulista.", 
+    link : "https://pt.wikipedia.org/wiki/Igor_Coronado",
+    tags :"armador pensador "
+
+    },
+    {  
+    titulo : "Rodrigo Garro",
+    descricao : "A jóia argentina do Corinthians é  um meia argentino que tem se destacado com a camisa do Corinthians. Chegando ao clube com a expectativa de ser uma das peças-chave do time, o jovem jogador tem demonstrado grande talento e potencial. Sua habilidade com a bola, visão de jogo e capacidade de criar jogadas perigosas o tornam um dos destaques da equipe. Com uma técnica apurada e uma personalidade forte dentro de campo, Garro tem conquistado o coração da Fiel Torcida e se consolidado como uma das promessas do futebol brasileiro..", 
+    link : "https://pt.wikipedia.org/wiki/Rodrigo_Garro",
+    tags:"joia argentina "
+
+    },
+    {
+    titulo : "Memphis Depay",
+    descricao : " Memphis Depay (Moordrecht, 13 de fevereiro de 1994) é um futebolista neerlandês que atua como centroavante. Recém contratado pelo Corinthians ", 
+    link : "https://pt.wikipedia.org/wiki/Memphis_Depay",
+    tags :"holanda goleador rapper"
+
+    },
+    {
+    titulo : "Thales Magno",
+    descricao : " Talles Magno Bacelar Martins (Rio de Janeiro, 26 de junho de 2002) é um futebolista brasileiro que atua como ponta-esquerda. Atualmente joga no Corinthians, emprestado pelo New York City.", 
+    link : "https://pt.wikipedia.org/wiki/Thales_Magno",
+    tags : "magico jovem raça"
+
+    }
+];
+
+ 
